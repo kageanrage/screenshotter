0ca669fe9bafe84fa5f7e1fbb439c8a9c4cb820d
kageanrage <kevinjpickett@gmail.com>
2017-06-15 13:14:04 +1000

    added files

---
+++ screenshotter.py
@@ -0,0 +1,19 @@
+# script to log in to web interface of cameras in home network and take screenshots at regular intervals, then store
+# these in a folder. Aim is to build up a database of photos capturing what parts of our home look like and how they
+# change over time. Particularly the kids rooms in the evenings around bed time.
+
+import selenium
+
+
+
+# Open web browser (selenium)
+# grab u/p from config file
+# log in to web browser (firefox or IE?)
+# go to first camera of interest
+# use built-in photo-capture button, or maximise and screenshot
+# rename image
+# move image
+# Do the same for the other cameras of interest
+# email me if any failures occur
+# create BAT file (in Windows) and schedule with Task Scheduler
+
